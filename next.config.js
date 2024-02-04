636bd8addedc9a8101ec1de37b107aa7e6fbddc5
Peter Yang <ipod4928@gmail.com>
2024-02-04 14:42:11 +0800

    modified next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const repo = 'techAndGuitar';
-const pageLink = 'https://petermyrobot.github.io/';
-const assetPrefix = `${pageLink}/${repo}/`;
-const basePath = `/${pageLink}/${repo}`;
 const nextConfig = {
-  assetPrefix: 'https://petermyrobot.github.io/techAndGuitar',
-  basePath: 'https://petermyrobot.github.io/techAndGuitar',
   output: 'export',
 };
 
