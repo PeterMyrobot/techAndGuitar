357461f8fbd5e30ce5e19ccc7b813da893f0a8fa
Peter Yang <ipod4928@gmail.com>
2024-02-04 13:53:36 +0800

    modified next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ['/techAndGuitar'] },
-  env: { PUBLIC_URL: 'https://petermyrobot.github.io/techAndGuitar' },
+  env: { PUBLIC_URL: 'https://techAndGuitar.com/techAndGuitar' },
+  basePath: '/techAndGuitar',
   output: 'export',
 };
 
