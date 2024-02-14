78a57505485ac132062be373d3b3098b35ff6dea
Wester Yang <wester_yang@trendmicro.com>
2024-02-14 21:19:36 +0800

    FretBoard: added note

--- src/app/LearnTheFretBoard/component/FretBoard.tsx
+++ src/app/LearnTheFretBoard/component/FretBoard.tsx
@@ -16,21 +16,30 @@ type TFretBoard = {
   notes: TNote[];
 };
 
+const getNote = (startNote: string, fret: number) => {
+  const index = (NOTE.indexOf(startNote) + fret) % NOTE.length;
+  return NOTE[index];
+};
+
 function FretBoard({ notes }: TFretBoard) {
   return (
     <div className="flex flex-col bg-yellow-700 h-[300px]">
-      {TUNING.map((note, index) => {
+      {TUNING.reverse().map((note, index) => {
         return (
           <div
             key={index}
             className=" flex before:absolute before:mt-[21px] before:bg-blue-500 before:h-2 before:w-full before:bg-gradient-to-b before:from-white before:to-gray-600 after:z-10 h-[50px]"
           >
-            {Array.from({ length: FRET }).map((_, i) => {
+            {Array.from({ length: FRET + 1 }).map((_, i) => {
               return (
                 <div
                   key={`${index}-${i}`}
                   className="grow border-r-4 border-gray-300 ] flex justify-center items-center "
-                ></div>
+                >
+                  <div className="rounded-full bg-cyan-400 opacity-70 font-white h-6 w-6 text-center border-white">
+                    {getNote(note, i)}
+                  </div>
+                </div>
               );
             })}
           </div>
