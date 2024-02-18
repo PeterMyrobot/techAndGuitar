1cb73df99fd6d45d478af188d1e703491a461f21
Peter Yang <ipod4928@gmail.com>
2024-02-18 21:09:46 +0800

    POC of learn the fret board

--- src/app/LearnTheFretBoard/component/FretBoard.tsx
+++ src/app/LearnTheFretBoard/component/FretBoard.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 
 const NOTE = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
 
-const TUNING = ['E', 'A', 'D', 'G', 'B', 'E'].reverse();
-
 const FRET_MARK = [3, 5, 7, 9];
 
-const FRET = 12;
-
-type TNote = {
+export type TNote = {
   string: number;
   fret: number;
   color: string;
@@ -17,49 +13,54 @@ type TNote = {
 
 type TFretBoard = {
   notes?: TNote[];
+  totalFret?: number;
+  stringTuning?: string[];
 };
 
-const getNote = (stringIdx: number, fretIdx: number) => {
-  const tuningString = TUNING[stringIdx];
+export const getNote = (stringIdx: number, fretIdx: number, stringTuning: string[]) => {
+  const tuningString = stringTuning[stringIdx];
   const index = (NOTE.indexOf(tuningString) + fretIdx) % NOTE.length;
   return NOTE[index];
 };
 
 const getFretPositionIndex = (stringIdx: number, fretIdx: number) => `${stringIdx}-${fretIdx}`;
 
-const NoteOnFretboard = ({ note }: { note: TNote }) => {
-  const { string, fret, color = 'bg-cyan-400', showingNote } = note;
-  const noteMark = getNote(string, fret);
-
-  return (
-    <div
-      className={`absolute rounded-full ${color} opacity-70 font-white w-8 aspect-square text-center leading-8  border-white border z-20`}
-    >
-      {showingNote ? noteMark : ''}
-    </div>
-  );
-};
-
 function FretBoard({
   notes = [{ string: 3, fret: 7, color: 'bg-violet-400', showingNote: true }],
+  totalFret = 12,
+  stringTuning = ['E', 'A', 'D', 'G', 'B', 'E'].reverse(),
 }: TFretBoard) {
   const notesMap = notes.reduce((map, note) => {
+    if (!note) return map;
     const notePosition = getFretPositionIndex(note.string, note.fret);
     map[notePosition] = note;
     return map;
   }, {} as Record<string, any>);
+
+  const NoteOnFretBoard = ({ note }: { note: TNote }) => {
+    const { string, fret, color = 'bg-cyan-400', showingNote } = note;
+    const noteMark = getNote(string, fret, stringTuning);
+
+    return (
+      <div
+        className={`absolute rounded-full ${color} opacity-70 font-white w-8 aspect-square text-center leading-8  border-white border z-20`}
+      >
+        {showingNote ? noteMark : ''}
+      </div>
+    );
+  };
   return (
     <div className="flex flex-col bg-yellow-700 h-[300px]">
-      {TUNING.map((_, stringIdx) => {
+      {stringTuning.map((_, stringIdx) => {
         return (
           <div
             key={stringIdx}
             className=" flex before:absolute before:mt-[21px]  before:z-10 before:h-2 before:w-full before:bg-gradient-to-b before:from-white before:to-gray-600 after:z-10 h-[50px]"
           >
-            {Array.from({ length: FRET + 1 }).map((_, fretIdx) => {
+            {Array.from({ length: totalFret + 1 }).map((_, fretIdx) => {
               const currentPosition = getFretPositionIndex(stringIdx, fretIdx);
               const isNoteMarked = currentPosition in notesMap;
-              const fretMark = stringIdx === 3 && FRET_MARK.includes(fretIdx);
+              const fretMark = stringIdx === 3 && FRET_MARK.includes(fretIdx % 12);
               const twelveMark = [2, 4].includes(stringIdx) && fretIdx === 12;
               const mark = fretMark || twelveMark;
 
@@ -71,7 +72,7 @@ function FretBoard({
                   {mark && (
                     <div className="w-6 bg-gray-300 rounded-full aspect-square absolute mt-[-50px]" />
                   )}
-                  {isNoteMarked && <NoteOnFretboard note={notesMap[currentPosition]} />}
+                  {isNoteMarked && <NoteOnFretBoard note={notesMap[currentPosition]} />}
                 </div>
               );
             })}
--- src/app/LearnTheFretBoard/page.tsx
+++ src/app/LearnTheFretBoard/page.tsx
@@ -1,11 +1,54 @@
-import FretBoard from './component/FretBoard';
+'use client';
+
+import { useState } from 'react';
+import FretBoard, { TNote, getNote } from './component/FretBoard';
 import NotePad from './component/NotePad';
+
 export default function LearnTheFretBoard() {
+  const fretAmount = 15;
+  const stringTuning = ['E', 'A', 'D', 'G', 'B', 'E'].reverse();
+  const [quiz, setQuiz] = useState<TNote>();
+  const generateQuiz = (): TNote => {
+    // generate random number between 0 and 5
+    const stringIdx = Math.floor(Math.random() * stringTuning.length);
+    const fretIdx = Math.floor(Math.random() * fretAmount);
+
+    return { string: stringIdx, fret: fretIdx, color: 'bg-rose-500', showingNote: false };
+  };
+
+  const generateNextQuiz = () => {
+    setQuiz(generateQuiz());
+  };
+
+  const answerClick = (note: string) => {
+    if (!quiz) return;
+    const { fret, string } = quiz;
+    console.log(getNote(string, fret, stringTuning), note);
+    if (note === getNote(string, fret, stringTuning)) {
+      setQuiz({ ...quiz, showingNote: true, color: 'bg-sky-500' });
+      setTimeout(() => {
+        generateNextQuiz();
+      }, 1000);
+    } else {
+      alert('Stupid!');
+    }
+  };
   return (
     <div className="bg-slate-500 h-screen">
       <h1 className="text-5xl text-center py-8">Learn The Fret Board</h1>
-      <FretBoard />
-      <NotePad />
+      <FretBoard notes={quiz ? [quiz] : []} totalFret={fretAmount} stringTuning={stringTuning} />
+      <NotePad onClick={answerClick} />
+      <div className="border border-stone-950 p-2">
+        <h1 className=" text-lg">What is this note?</h1>
+        <p>string:{quiz?.string}</p>
+        <p>fret: {quiz?.fret}</p>
+        <button
+          className="rounded-lg bg-white  border border-black text-blue-950  px-4"
+          onClick={generateNextQuiz}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
