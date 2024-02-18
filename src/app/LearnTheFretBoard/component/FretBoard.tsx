8b45283530ed67a2974893e5f18001acd68df193
Peter Yang <ipod4928@gmail.com>
2024-02-18 19:56:07 +0800

    added fret board marker to the fret board

--- src/app/LearnTheFretBoard/component/FretBoard.tsx
+++ src/app/LearnTheFretBoard/component/FretBoard.tsx
@@ -4,6 +4,8 @@ const NOTE = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
 
 const TUNING = ['E', 'A', 'D', 'G', 'B', 'E'].reverse();
 
+const FRET_MARK = [3, 5, 7, 9];
+
 const FRET = 12;
 
 type TNote = {
@@ -28,11 +30,10 @@ const getFretPositionIndex = (stringIdx: number, fretIdx: number) => `${stringId
 const NoteOnFretboard = ({ note }: { note: TNote }) => {
   const { string, fret, color = 'bg-cyan-400', showingNote } = note;
   const noteMark = getNote(string, fret);
-  console.log(string, fret, color, showingNote, noteMark);
 
   return (
     <div
-      className={`absolute rounded-full ${color} opacity-70 font-white w-8 aspect-square text-center leading-8  border-white border`}
+      className={`absolute rounded-full ${color} opacity-70 font-white w-8 aspect-square text-center leading-8  border-white border z-20`}
     >
       {showingNote ? noteMark : ''}
     </div>
@@ -47,25 +48,30 @@ function FretBoard({
     map[notePosition] = note;
     return map;
   }, {} as Record<string, any>);
-  console.log({ notesMap });
   return (
     <div className="flex flex-col bg-yellow-700 h-[300px]">
       {TUNING.map((_, stringIdx) => {
         return (
           <div
             key={stringIdx}
-            className=" flex before:absolute before:mt-[21px]  before:h-2 before:w-full before:bg-gradient-to-b before:from-white before:to-gray-600 after:z-10 h-[50px]"
+            className=" flex before:absolute before:mt-[21px]  before:z-10 before:h-2 before:w-full before:bg-gradient-to-b before:from-white before:to-gray-600 after:z-10 h-[50px]"
           >
             {Array.from({ length: FRET + 1 }).map((_, fretIdx) => {
               const currentPosition = getFretPositionIndex(stringIdx, fretIdx);
-              const isMarked = currentPosition in notesMap;
+              const isNoteMarked = currentPosition in notesMap;
+              const fretMark = stringIdx === 3 && FRET_MARK.includes(fretIdx);
+              const twelveMark = [2, 4].includes(stringIdx) && fretIdx === 12;
+              const mark = fretMark || twelveMark;
 
               return (
                 <div
                   key={currentPosition}
-                  className="grow border-r-4 border-gray-300 flex justify-center items-center "
+                  className="grow border-r-4 border-gray-300 flex justify-center items-center relative first:bg-yellow-950"
                 >
-                  {isMarked && <NoteOnFretboard note={notesMap[currentPosition]} />}
+                  {mark && (
+                    <div className="w-6 bg-gray-300 rounded-full aspect-square absolute mt-[-50px]" />
+                  )}
+                  {isNoteMarked && <NoteOnFretboard note={notesMap[currentPosition]} />}
                 </div>
               );
             })}
